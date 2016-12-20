0f34a3c821656a75d4211445ad13686cdbd1a636
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2016-12-20 10:31:51 +0800

    implement is_member_of?(group)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217031424) do
+ActiveRecord::Schema.define(version: 20161219162051) do
+
+  create_table "group_relationships", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
